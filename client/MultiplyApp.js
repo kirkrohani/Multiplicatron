8098dedd9ac38ad026efb65406e87a68e5e826ca
Kirk Rohani <kirk.rohani@gmail.com>
2017-04-21 22:07:09 -0500

    completed styling and added edge case to multiply decimals

--- client/MultiplyApp.js
+++ client/MultiplyApp.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log('At the beginning of MultiplyApp.js');
 
   const MathOperations = require("./MathOperations");
   const multiplyNumbers = new MathOperations().multiply;
@@ -9,14 +8,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   class MultiplyAppManager {
 
     createInputField(inputFieldsCount) {
-      
       const newInputField = document.createElement("input");
       newInputField.type = "text";
       newInputField.size = "3";
 
       const newDiv = document.createElement("div");  
       newDiv.id = `number${++inputFieldsCount}`;
-
       newDiv.appendChild(newInputField);
       
       return newDiv;
@@ -26,32 +23,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
     connectEventHandlers() {
       document.getElementById("calculate_button").addEventListener("click", this.onSubmit.bind(this));
       document.getElementById("multiply_more_numbers").addEventListener("click", this.displayMoreInputs.bind(this));
+      document.getElementById("turn_off_link").addEventListener("click", this.turnOffBlinking.bind(this));
     }
 
+    turnOffBlinking(event) {
+      document.getElementsByClassName("flash")[0].style.animationPlayState = "paused";
+    }
 
     onSubmit(event) {
       event.preventDefault();
-      console.log('User clicked on submit');
 
       let allInputs = Array.prototype.slice.call(document.querySelectorAll("input"))
-        .map( node => parseInt(node.value) )  
+        .map( node => parseFloat(node.value) || parseInt(node.value) )  
         .filter( num => !isNaN(num) );
     
       const total = allInputs.length >= 2 ? multiplyNumbers(allInputs) : NaN;
-      document.querySelector("#total span").textContent = total;
-      
+      document.querySelector("#total span").textContent = total; 
     }
 
     displayMoreInputs(event) {
       event.preventDefault();
-      console.log('User clicked on more inputs');
 
       const currentNumberOfInputs = document.querySelectorAll("input").length;
       const parentNode = document.querySelector("#multiply_numbers");
       const referenceNode = document.querySelector("#total");
 
       parentNode.insertBefore(this.createInputField(currentNumberOfInputs), referenceNode);
-      
     }
   }
   
--- client/bundle.js
+++ client/bundle.js
@@ -11,7 +11,6 @@ module.exports = MathOperations;
 
 },{}],2:[function(require,module,exports){
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log('At the beginning of MultiplyApp.js');
 
   const MathOperations = require("./MathOperations");
   const multiplyNumbers = new MathOperations().multiply;
@@ -21,14 +20,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
   class MultiplyAppManager {
 
     createInputField(inputFieldsCount) {
-      
       const newInputField = document.createElement("input");
       newInputField.type = "text";
       newInputField.size = "3";
 
       const newDiv = document.createElement("div");  
       newDiv.id = `number${++inputFieldsCount}`;
-
       newDiv.appendChild(newInputField);
       
       return newDiv;
@@ -38,32 +35,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
     connectEventHandlers() {
       document.getElementById("calculate_button").addEventListener("click", this.onSubmit.bind(this));
       document.getElementById("multiply_more_numbers").addEventListener("click", this.displayMoreInputs.bind(this));
+      document.getElementById("turn_off_link").addEventListener("click", this.turnOffBlinking.bind(this));
     }
 
+    turnOffBlinking(event) {
+      document.getElementsByClassName("flash")[0].style.animationPlayState = "paused";
+    }
 
     onSubmit(event) {
       event.preventDefault();
-      console.log('User clicked on submit');
 
       let allInputs = Array.prototype.slice.call(document.querySelectorAll("input"))
-        .map( node => parseInt(node.value) )  
+        .map( node => parseFloat(node.value) || parseInt(node.value) )  
         .filter( num => !isNaN(num) );
     
       const total = allInputs.length >= 2 ? multiplyNumbers(allInputs) : NaN;
-      document.querySelector("#total span").textContent = total;
-      
+      document.querySelector("#total span").textContent = total; 
     }
 
     displayMoreInputs(event) {
       event.preventDefault();
-      console.log('User clicked on more inputs');
 
       const currentNumberOfInputs = document.querySelectorAll("input").length;
       const parentNode = document.querySelector("#multiply_numbers");
       const referenceNode = document.querySelector("#total");
 
       parentNode.insertBefore(this.createInputField(currentNumberOfInputs), referenceNode);
-      
     }
   }
   
